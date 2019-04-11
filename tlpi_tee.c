8c4683a567362eacdb45433e3251f06b491289b3
lukerobbins2112 <lukerobbins2112@gmail.com>
2019-04-11 12:42:31 -0500

    Updated tee program to use the -a flag with getopts. Protocol is that flags come first, since it uses optind

--- tlpi_tee.c
+++ tlpi_tee.c
@@ -16,10 +16,27 @@ int main(int argc, char ** argv){
   if (argc < 2)
     printf("err - need file name\n");
 
+  
   // Open file for output
-  int flags = O_WRONLY | O_CREAT | O_TRUNC;
+  int flags = O_WRONLY | O_CREAT;
+
+  // If no '-a' flag, truncate - otherwise append
+  if (getopt(argc, argv, "a") == 'a'){
+     flags = flags | O_APPEND;
+     printf("Append mode\n");
+  }
+  else{    
+    flags = flags | O_TRUNC;
+    printf("Truncate mode\n");
+  }
+
+  // Set mode 
   mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
-  int fd = open(argv[1], flags, mode);
+
+  // Open the file
+  // optind is set by getopt - used to specify non-option arguments
+  // Using this, non-option arguments have to come AFTER all option (flag) args
+  int fd = open(argv[optind], flags, mode);
 
   
   // Read from stdin while not EOF
