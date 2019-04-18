af3922c4d91ac78f1764eb9c1ad53c9f7524c317
lukerobbins2112 <lukerobbins2112@gmail.com>
2019-04-18 16:24:03 -0500

    Added my_dup.c, implementing dup() with fcntl

---
+++ chapter7/my_dup.c
@@ -0,0 +1,39 @@
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define BUF_SIZE 1024
+#define FD_MIN 10
+
+int myDup(int originalFd){
+
+
+  return fcntl(originalFd, F_DUPFD, originalFd);
+
+}
+
+int main(){
+
+  char buffer[BUF_SIZE];
+  int bytesRead = 0;
+
+  int copyFD = myDup(STDOUT_FILENO);
+  if (copyFD == -1) perror("dup");
+
+  while((bytesRead = read(STDIN_FILENO, buffer, BUF_SIZE)) > 0){
+
+    // Null-terminate string
+    buffer[bytesRead] = '\0';
+
+    if (strcmp(buffer, "\n") == 0) break;
+
+    write(copyFD, buffer, bytesRead);
+  }
+
+
+  return 0;
+
+}
