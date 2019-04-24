2c53b56cddd0aa5497f655af30a089d753f6b433
lukerobbins2112 <lukerobbins2112@gmail.com>
2019-04-24 13:44:57 -0500

    Added chapter12 dir and tlpi_uname.c file

---
+++ chapter12/tlpi_uname.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+
+  struct utsname uts;
+
+  if (uname(&uts) == -1)
+    perror("uname");
+
+  printf("Node name: %s\n", uts.nodename);
+  printf("System name: %s\n", uts.sysname);
+  printf("Release: %s\n", uts.release);
+  printf("Version: %s\n", uts.version);
+  printf("Machine: %s\n", uts.machine);
+
+#ifdef _GNU_SOURCE
+  printf("Domain name: %s\n", uts.domainname);
+#endif
+ exit(EXIT_SUCCESS);
+
+}
